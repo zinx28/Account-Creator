3cb606653d01a414bff6f9333e47417f5c19d2da
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-02-28 22:55:56 +0000

    added a note and formatted it

--- commands/signup.js
+++ commands/signup.js
@@ -5,151 +5,152 @@ const bcrypt = require("bcrypt");
 const config = require(`../config.json`);
 //Connect
 mongoose.connect(`${config.mon}`, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
-	console.log(`${config.log} Connected to the database!`);
+    console.log(`${config.log} Connected to the database!`);
 }).catch((err) => {
-	console.log(err);
+    console.log(err);
 })
 
 const Data = require('../models/user')
 const Data1 = require('../models/a')
 const Data2 = require('../models/cc')
 const Data3 = require('../models/f')
+// if alot of people is using this then fix the server to dm command since if there dms are off then it gives errors
 module.exports.run = async (client, message, args) => {
-	if (message.channel.type === "dm") {
-		Data.findOne({
-			userId: message.author.id
-		}, (err, Getdata) => {
-			const reason = args.slice(1).join(" ") || null
-      if (!Getdata) {}else{
-				return message.channel.send("Failed to create account. The username may already be taken or you might already have an account")
-			}
-			if (!reason == ! null) return message.channel.send(`Failed to create account. The username may already be taken or you might already have an account.`);
+    if (message.channel.type === "dm") {
+        Data.findOne({
+            userId: message.author.id
+        }, (err, Getdata) => {
+            const reason = args.slice(1).join(" ") || null
+            if (!Getdata) { } else {
+                return message.channel.send("Failed to create account. The username may already be taken or you might already have an account")
+            }
+            if (!reason == ! null) return message.channel.send(`Failed to create account. The username may already be taken or you might already have an account.`);
 
-			Data.findOne({ 'displayName': reason }, (err1, DataBOI1) => {
-				if (DataBOI1) {
-					if (DataBOI1.displayName == reason.toString()) {
-						ReadyBOI = false
-						return message.channel.send(`Username is already in use please try again (run ${congig.prefix}setup-user again)`)
-					}
-				} else {
+            Data.findOne({ 'displayName': reason }, (err1, DataBOI1) => {
+                if (DataBOI1) {
+                    if (DataBOI1.displayName == reason.toString()) {
+                        ReadyBOI = false
+                        return message.channel.send(`Username is already in use please try again (run ${congig.prefix}setup-user again)`)
+                    }
+                } else {
 
-					const idBOI = crypto.randomBytes(16).toString('hex')
-					var ReadyBOI
-					ReadyBOI = true
-					const newMadeDataF = new Data3({
-						id: idBOI,
-						userId: message.author.id
-					})
-					newMadeDataF.save().catch(err1 => console.log(err1))
+                    const idBOI = crypto.randomBytes(16).toString('hex')
+                    var ReadyBOI
+                    ReadyBOI = true
+                    const newMadeDataF = new Data3({
+                        id: idBOI,
+                        userId: message.author.id
+                    })
+                    newMadeDataF.save().catch(err1 => console.log(err1))
 
-					//User
-					const newMadeDataUSER = new Data({
-						id: idBOI,
-						createdAt: new Date(),
-						allowsGifts: true,
-						userId: message.author.id,
-						displayName: reason,
-						email: reason + "@Fortnite.Dev",
-						password: bcrypt.hashSync(idBOI, bcrypt.genSaltSync(10))
-					})
-					newMadeDataUSER.save().catch(err1 => console.log(err1))
+                    //User
+                    const newMadeDataUSER = new Data({
+                        id: idBOI,
+                        createdAt: new Date(),
+                        allowsGifts: true,
+                        userId: message.author.id,
+                        displayName: reason,
+                        email: reason + "@Fortnite.Dev",
+                        password: bcrypt.hashSync(idBOI, bcrypt.genSaltSync(10))
+                    })
+                    newMadeDataUSER.save().catch(err1 => console.log(err1))
 
-					//CC
-					const newMadeDataCC = new Data2({
-						id: idBOI,
-						userId: message.author.id,
-						mtxplatform: "EpicPC"
-					})
-					newMadeDataCC.save().catch(err1 => console.log(err1))
+                    //CC
+                    const newMadeDataCC = new Data2({
+                        id: idBOI,
+                        userId: message.author.id,
+                        mtxplatform: "EpicPC"
+                    })
+                    newMadeDataCC.save().catch(err1 => console.log(err1))
 
-					//Athena
-					const newMadeDataA = new Data1({
-						id: idBOI,
-						userId: message.author.id,
-					})
-					newMadeDataA.save().catch(err1 => console.log(err1))
-					// gotta work on this message
-					return message.channel.send(`Account Created Email : ${reason + "@Fortnite.Dev"} Password: ${idBOI}`)
-				}
-			}
-			)
+                    //Athena
+                    const newMadeDataA = new Data1({
+                        id: idBOI,
+                        userId: message.author.id,
+                    })
+                    newMadeDataA.save().catch(err1 => console.log(err1))
+                    // gotta work on this message
+                    return message.channel.send(`Account Created Email : ${reason + "@Fortnite.Dev"} Password: ${idBOI}`)
+                }
+            }
+            )
 
-		})
-	} else {
-	
-	Data.findOne({
-			userId: message.author.id
-		}, (err, Getdata) => {
-			const reason = args.slice(1).join(" ") || null
-      if (!Getdata) {}else{
-				return message.channel.send("Failed to create account. The username may already be taken or you might already have an account")
-			}
-			if (!reason == ! null) return message.channel.send(`Failed to create account. The username may already be taken or you might already have an account.`);
-			Data.findOne({ 'displayName': reason }, (err1, DataBOI1) => {
-				if (DataBOI1) {
-					if (DataBOI1.displayName == reason.toString()) {
-						ReadyBOI = false
-						return message.member.send(`Username is already in use please try again (run ${congig.prefix}setup-user again)`)
-					}
-				} else {
+        })
+    } else {
 
-					const idBOI = crypto.randomBytes(16).toString('hex')
-					var ReadyBOI
-					ReadyBOI = true
-					const newMadeDataF = new Data3({
-						id: idBOI,
-						userId: message.author.id
-					})
-					newMadeDataF.save().catch(err1 => console.log(err1))
+        Data.findOne({
+            userId: message.author.id
+        }, (err, Getdata) => {
+            const reason = args.slice(1).join(" ") || null
+            if (!Getdata) { } else {
+                return message.channel.send("Failed to create account. The username may already be taken or you might already have an account")
+            }
+            if (!reason == ! null) return message.channel.send(`Failed to create account. The username may already be taken or you might already have an account.`);
+            Data.findOne({ 'displayName': reason }, (err1, DataBOI1) => {
+                if (DataBOI1) {
+                    if (DataBOI1.displayName == reason.toString()) {
+                        ReadyBOI = false
+                        return message.member.send(`Username is already in use please try again (run ${congig.prefix}setup-user again)`)
+                    }
+                } else {
 
-					//User
-					const newMadeDataUSER = new Data({
-						id: idBOI,
-						createdAt: new Date(),
-						allowsGifts: true,
-						userId: message.author.id,
-						displayName: reason,
-						email: reason + "@Fortnite.Dev",
-						password: bcrypt.hashSync(idBOI, bcrypt.genSaltSync(10))
-					})
-					newMadeDataUSER.save().catch(err1 => console.log(err1))
+                    const idBOI = crypto.randomBytes(16).toString('hex')
+                    var ReadyBOI
+                    ReadyBOI = true
+                    const newMadeDataF = new Data3({
+                        id: idBOI,
+                        userId: message.author.id
+                    })
+                    newMadeDataF.save().catch(err1 => console.log(err1))
 
-					//CC
-					const newMadeDataCC = new Data2({
-						id: idBOI,
-						userId: message.author.id,
-						mtxplatform: "EpicPC"
-					})
-					newMadeDataCC.save().catch(err1 => console.log(err1))
+                    //User
+                    const newMadeDataUSER = new Data({
+                        id: idBOI,
+                        createdAt: new Date(),
+                        allowsGifts: true,
+                        userId: message.author.id,
+                        displayName: reason,
+                        email: reason + "@Fortnite.Dev",
+                        password: bcrypt.hashSync(idBOI, bcrypt.genSaltSync(10))
+                    })
+                    newMadeDataUSER.save().catch(err1 => console.log(err1))
 
-					//Athena
-					const newMadeDataA = new Data1({
-						id: idBOI,
-						userId: message.author.id,
-					})
-           
-					 
-					// I Gotta Add DM Thingy (Make Sure Your Dms are on or bot will crash i think)
-					newMadeDataA.save().catch(err1 => console.log(err1))
-					// gotta work on this message
-					message.member.send(`Account Created Email : ${reason + "@Fortnite.Dev"} Password: ${idBOI}`)
-					message.reply('I sent you your info!');
-				
+                    //CC
+                    const newMadeDataCC = new Data2({
+                        id: idBOI,
+                        userId: message.author.id,
+                        mtxplatform: "EpicPC"
+                    })
+                    newMadeDataCC.save().catch(err1 => console.log(err1))
 
-				}
+                    //Athena
+                    const newMadeDataA = new Data1({
+                        id: idBOI,
+                        userId: message.author.id,
+                    })
 
-})
 
-		})
-	}
+                    // I Gotta Add DM Thingy (Make Sure Your Dms are on or bot will crash i think)
+                    newMadeDataA.save().catch(err1 => console.log(err1))
+                    // gotta work on this message
+                    message.member.send(`Account Created Email : ${reason + "@Fortnite.Dev"} Password: ${idBOI}`)
+                    message.reply('I sent you your info!');
+
+
+                }
+
+            })
+
+        })
+    }
 }
 module.exports.config = {
-	name: "setup-user",
-	description: "",
-	usage: "!setup-user",
-	accessableby: "Members",
-	aliases: ["user", "signup"]
+    name: "setup-user",
+    description: "",
+    usage: "!setup-user",
+    accessableby: "Members",
+    aliases: ["user", "signup"]
 }
