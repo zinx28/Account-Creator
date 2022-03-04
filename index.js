0b664c93c6330a1f5895cc617e0ad950acd24f9d
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-03-04 20:27:37 +0000

    this isnt really used

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!';
 const fs = require('fs');
 client.cmdlist = new Discord.Collection();
 const config = require(`./config.json`);
