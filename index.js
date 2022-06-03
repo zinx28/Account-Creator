a775ceef2396ec5c8e98fdb4fa2c3136d4651f43
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-06-03 21:01:59 +0100

    fixed the mongoose isnt defined or something

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const mongoose = require('mongoose');
 const fs = require('fs');
 const config = require(`./config.json`);
 mongoose.connect(`${config.mon}`, {
